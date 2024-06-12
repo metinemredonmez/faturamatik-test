5c1ca6a2f2e3b7a63166699a78fddca5a7c0f415
Metin Emre <54211029+metinemredonmez@users.noreply.github.com>
2024-06-13 01:25:28 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins:[
+  plugins: [
+    'react-native-reanimated/plugin',
     [
-      'react-native-reanimated/plugin',
-    ],
-    [
-      require.resolve('babel-plugin-module-resolver'),
+      'module-resolver',
       {
         cwd: 'babelrc',
-        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        extensions: ['.ts', '.tsx', '.js', 'json', 'jsx', '.ios.js', '.android.js'],
         alias: {
           '@': './src'
         }
       }
     ],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
   ]
 };
